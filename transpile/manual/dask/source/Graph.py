e554eac1d59d1082c84b4eb63369fbc9a2912e87
Kenneth Dirker <kennethdirker@hotmail.nl>
2024-11-12 18:15:00 +0100

    Step automatically sets attributes

--- transpile/manual/dask/source/Graph.py
+++ transpile/manual/dask/source/Graph.py
@@ -1,18 +1,49 @@
 # Standard modules
 from typing import Dict, List
 
+
 class Step:
+    """"""
+    step_attrs = [
+        # "id",
+        # "baseCommand",
+        "requirements",
+        "hints",
+        "label",
+        "stderr",
+        "stdout",
+        "stdin",
+        "successCodes",
+        "permanentFailCodes",
+        "temporaryFailCodes",
+        "save",
+        "extension_fields",
+        "intents"
+    ]
+
+
+    def add_attrs(self, **kwargs):
+        """"""
+        for attr, value in kwargs.items():
+            if attr in step_attrs:
+                setattr(self, attr, value)
+            else:
+                # TODO Decide if debugging only
+                raise Exception(f"{attr} is not a valid step attribute.")
+
     def __init__(
             self,
             id: str,
             baseCommand: List[str],
-            inputs: Dict[str, dict],
-            outputs: Dict[str, dict]
+            **kwargs
+            # inputs: Dict[str, dict],
+            # outputs: Dict[str, dict]
         ):
         self.id: int = id
         self.baseCommand: List[str] = baseCommand
-        self.inputs: Dict[str, dict] = inputs
-        self.outputs: Dict[str, dict] = outputs
+        self.add_attrs(kwargs)
+        # self.inputs: Dict[str, dict] = inputs
+        # self.outputs: Dict[str, dict] = outputs
         # self.requirements
         # self.hints
         # self.label
@@ -46,14 +77,15 @@ class DependencyNode:
 
 class DependencyGraph:
     def __init__(self, grouping: bool = False):
-        self.grouping: bool = grouping
-        self.nodes: Dict[int, DependencyNode] = {}
-
         #TODO Which of the following 2?
         self.roots: List[DependencyNode] = []
         # self.roots: List[int] = []
 
+        self.grouping: bool = grouping
+        self.nodes: Dict[int, DependencyNode] = {}
         self.dependencies = {}  # {child_id: [parent_ids], ...}
+
+
 
 
     def add_node(
@@ -74,8 +106,17 @@ class DependencyGraph:
                     self.dependencies[node.id].append(parents)
                 else:
                     self.dependencies[node.id] = [parents]
+
+                # TODO WIP
+
+                
+                ...
+
+
+
             elif isinstance(parents, list):
                 #TODO multiple parents
+                # TODO WIP
                 pass
             else:
                 raise Exception(f"Invalid parameter type: parents({type(parents)}).")
