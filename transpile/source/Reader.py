3c24573942b66c6330a81ccc91ac3c7119906b87
Kenneth Dirker <kennethdirker@hotmail.nl>
2025-01-28 18:07:39 +0100

    Expand node loading

--- transpile/source/Reader.py
+++ transpile/source/Reader.py
@@ -7,8 +7,9 @@ from typing import Any, Optional, List, Dict
 from cwl_utils.parser import load_document_by_uri, cwl_version
 
 # Local modules 
-import datastructures as ds
-from Utils import retrieve_attr
+# import datastructures as ds
+from datastructures import Step, Node, Graph
+# from Utils import getattr_or_none, NoneType
 
 
 class Reader:
@@ -17,22 +18,22 @@ class Reader:
     Creating of dependency graph
     """
     # Reader workflow:
-    # Read workflow file 
-    # Create dependency graph with Process objects as nodes. 
-    # At the same time: Validate the workflow  
-    # The dependency graph is returned.
+    # 1. Read workflow file and convert to Python object.
+    # 2. Validate the workflow  
+    # 3. Create dependency graph with Process objects as nodes. 
+    # 4. Return dependency graph.
 
-    def __init__(self):
-        self._new_id = 0     # Used to assign IDs to graph nodes
+    def __init__(self, starting_id: int = 0):
+        self._next_id = starting_id     # Used to assign IDs to graph nodes
 
 
-    def _create_node_id(self) -> int:
-        id = self._new_id
-        self._new_id += 1
+    def _new_node_id(self) -> int:
+        id = self._next_id
+        self._next_id += 1
         return id
 
 
-    def load_cwl_step(self, path: Path) -> ds.Node:        
+    def load_step_node(self, path: Path) -> Node:        
         # Load CWL step file into an object 
         if not path.is_file():
             raise Exception(f"{path} is not a file.")
@@ -41,26 +42,35 @@ class Reader:
         cwl_object = load_document_by_uri(path)
 
         # Check whether CWL file really is a CommandLineTool
-        class_ = retrieve_attr(cwl_object, "class_")
-        if class_ not in "CommandLineTool":
+        class_ = getattr(cwl_object, "class_")
+        if "CommandLineTool" not in class_:
+            # TODO: Update exception type
             raise Exception(f"File at path {path} not a command line tool.")
 
-        # 
-        step_node = ds.Node()
-        id = retrieve_attr(cwl_object, "id")
-        baseCommand = retrieve_attr(cwl_object, "baseCommand")
-        inputs = retrieve_attr(cwl_object, "inputs")
-        outputs = retrieve_attr(cwl_object, "outputs")
+        tool_id = getattr(cwl_object, "id")
+        baseCommand = getattr(cwl_object, "baseCommand")
+        inputs = getattr(cwl_object, "inputs")
+        outputs = getattr(cwl_object, "outputs")
         parents = ...
-        #TODO WIP
-        return step_node
+
+        step = Step(
+            tool_id = tool_id,
+            baseCommand = baseCommand,
+            # TODO wip
+        )
+
+        return Node(
+            id = self._new_node_id(),
+            parents = parents,
+            steps = [step]
+        )
 
     
-    def load_cwl_workflow(self):
+    def load_workflow_graph(self):
         raise NotImplementedError()
 
 
-    def load_cwl(self, path: Path) -> ds.Graph:
+    def load_cwl(self, path: Path) -> Graph:
         # Load CWL step file into an object 
         if not path.is_file():
             raise Exception(f"{path} is not a file.")
@@ -69,12 +79,12 @@ class Reader:
             raise Exception(f"Wrong CWL version: {path} is not v1.2, but {v}.")
         cwl_object = load_document_by_uri(path)
 
-        graph = ds.Graph()
-        file_class = retrieve_attr(cwl_object, "class_")
+        graph = Graph()
+        file_class = getattr(cwl_object, "class_")
         if file_class in "CommandLineTool":
             # Add node to graph
-            node = self.load_cwl_step(path)
-            graph.add_node(node, parents)
+            node = self.load_step_node(path)
+            graph.add_node(node, path)
         elif file_class in "workflow":
             #TODO Breadth-first walk CWL steps/workflows
             raise NotImplementedError()
