af0bb02c0611c28ecfaba85663dbfd268b56f67b
Kenneth Dirker <kennethdirker@hotmail.nl>
2025-04-10 00:00:42 +0200

    Add workflow that doesn't require an internet connection

---
+++ transpile/PWF/PWF/example/no_internet/concat.py
@@ -0,0 +1,39 @@
+from PWF.src.CommandLineTool import BaseCommandLineTool
+
+class Concat(BaseCommandLineTool):
+
+    def set_metadata(self):
+        self.label = "concat"
+        self.doc = "Concatenate 2 strings"
+
+    
+    def set_inputs(self):
+        self.inputs = {
+            "left": {
+                "type": "string",
+                "position": 0
+            },
+            "right": {
+                "type": "string",
+                "position": 1
+            }
+        }
+
+
+    def set_outputs(self):
+        self.outputs = {
+            "output": {
+                "type": "file",
+                "glob": "concat.out"
+            }
+        }
+
+
+    # FIXME: Better function name
+    def set_base_command(self):
+        self.base_command: str | list[str] = ["echo", ">concat.out"]
+    
+    
+if __name__ == "__main__":
+    Concat(main=True)
+    
---
+++ transpile/PWF/PWF/example/no_internet/workflow.py
@@ -0,0 +1,75 @@
+from PWF.src.Workflow import BaseWorkflow
+
+class ProcessImages(BaseWorkflow):
+
+    def set_metadata(self):
+        self.label = "process_images"
+
+    
+    def set_inputs(self):
+        # FIXME make input ids globally unique to ensure sub-processes from
+        # overwriting each other. Use process id?
+        self.inputs = {
+            "animal_file": {
+                "type": "file"
+            },
+            "bodypart_file": {
+                "type": "file"
+            }
+        }
+
+
+    def set_outputs(self):
+        # FIXME make output ids globally unique to ensure sub-processes from
+        # overwriting each other. Use process id?
+        self.outputs = {
+            "animal_bodypart": {
+                "type": "str",
+                "outputSource": "concat_strings/output"
+            }
+        }
+
+
+    def set_steps(self):
+        # TODO Complete steps
+        self.steps = {
+            "get_animal": {
+                "in": {
+                    "file_path": {
+                        "source": "animal_file"
+                    }
+                },
+                "out": "output",
+                "run": "cat.py",
+                "label": "get_animal"
+            },
+            "get_bodypart": {
+                "in": {
+                    "file_path": {
+                        "source": "bodypart_file"
+                    }
+                },
+                "out": "output",
+                "run": "cat.py",
+                "label": "get_bodypart"
+            },
+            "concat_strings": {
+                "in": {
+                    "left": {
+                        "source": "get_animal/output"
+                    },
+                    "right": {
+                        "source": "get_bodypart/output"
+                    }
+                },
+                "out": "output",
+                "run": "concat.py",
+                "label": "concatenate_strings"
+            },
+
+        }
+    
+    
+if __name__ == "__main__":
+    ProcessImages(main=True)
+    
