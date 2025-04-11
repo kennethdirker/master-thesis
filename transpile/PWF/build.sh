4d142bc495a52512e31315e6ab6f0ff35bb91c32
Kenneth Dirker <kennethdirker@hotmail.nl>
2025-04-11 20:00:14 +0200

    Update env building

--- transpile/PWF/build.sh
+++ transpile/PWF/build.sh
@@ -1,4 +1,5 @@
-# python -m venv env
-# pip install -r requirements.txt
+python3.11 -m venv env
+pip install -r requirements.txt
+pip install -e .
 source ./env/bin/activate
 python -m pip install -e .
