a08cd06941109f5bef19c858ab99cbf19ee7ed79
Kenneth Dirker <kennethdirker@hotmail.nl>
2025-10-01 22:10:51 +0200

    Make FileObject use absolute paths

--- transpile/PWF/PWF/src/utils.py
+++ transpile/PWF/PWF/src/utils.py
@@ -8,7 +8,8 @@ class Absent:
     Used to indictate the absence of a runtime input. Useful when None is a
     valid value for an argument.
     """
-    pass
+    def __repr__(self) -> str:
+        return "Absent(None)"
 
 
 # def get_or_default(d: dict, default_value: Any) -> Any:
@@ -36,7 +37,7 @@ class FileObject:
     nameext: str = ""
     
     def __init__(self, file_path: str):
-        path: Path = Path(file_path)
+        path: Path = Path(file_path).resolve()
         self.path = str(path)
         self.basename = path.name
         self.dirname = str(path.parent)
@@ -45,3 +46,6 @@ class FileObject:
 
     def __str__(self) -> str:
         return self.path
+    
+    def __repr__(self) -> str:
+        return f"FileObject({self.path})"
