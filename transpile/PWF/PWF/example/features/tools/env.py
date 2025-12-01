269fdb0be11883cdcdc2798da9e080bdbb2280c4
Kenneth Dirker <kennethdirker@hotmail.nl>
2025-12-01 22:21:22 +0100

    Add tool to test EnvVarRequirement / unbound inputs

---
+++ transpile/PWF/PWF/example/features/tools/env.py
@@ -0,0 +1,31 @@
+from PWF.src.commandlinetool import BaseCommandLineTool
+
+class env_PWF(BaseCommandLineTool):
+
+	def set_metadata(self):
+		self.metadata = {}
+
+	def set_inputs(self):
+		self.inputs = {
+			"message": {
+				"type": "string",
+			},
+		}
+
+	def set_outputs(self):
+		self.outputs = {}
+
+	def set_base_command(self):
+		self.base_command = [
+			"env",
+		]
+
+	def set_requirements(self):
+		self.requirements = {
+			"EnvVarRequirement": {
+				"HELLO": "$(inputs.message)",
+			},
+		}
+
+if __name__ == "__main__":
+	env_PWF()
