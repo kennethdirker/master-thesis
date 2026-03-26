7bedde2a139a7e877de9ae954f1644d80a1e9fa2
Kenneth Dirker <kennethdirker@hotmail.nl>
2026-03-26 16:33:38 +0100

    Tidy up

--- transpile/PWF (copy)/PWF/example/cwl_simple_workflow/templates/CommandLineTool.cwl
+++
@@ -1,7 +0,0 @@
-class: CommandLineTool
-cwlVersion: v1.2
-id: fillhere
-baseCommand: []
-inputs: []
-outputs: []
-label: fillhere
--- transpile/PWF (copy)/build.sh
+++
@@ -1,5 +0,0 @@
-python3.11 -m venv env
-pip install -r requirements.txt
-pip install -e .
-source ./env/bin/activate
-python -m pip install -e .
