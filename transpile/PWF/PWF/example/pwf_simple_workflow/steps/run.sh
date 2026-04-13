184a5c4aa5ed840657fcf0c29743199402fe5099
Kenneth Dirker <kennethdirker@hotmail.nl>
2026-04-13 16:15:25 +0200

    Preserve tmp folder

--- transpile/PWF/PWF/example/pwf_simple_workflow/steps/run.sh
+++ transpile/PWF/PWF/example/pwf_simple_workflow/steps/run.sh
@@ -1,6 +1,7 @@
 OUT=out.png
 NOISE=no_noise_image.fits
 NOISEOUT=no_noise_out.png
+TMP=tmp_dir
 
 if test -f $OUT; then
     rm $OUT
@@ -11,9 +12,12 @@ fi
 if test -f $NOISEOUT; then
     rm $NOISEOUT
 fi
+if test -d $TMP; then
+    rm -rf $TMP
+fi
 
 # Execute the tools simulating the workflow in the workflow directory
 # python download_images.py download_images.yaml    < Doesn't work because url is broken
-python imageplotter.py -y imageplotter.yaml
-python noiseremover.py -y noiseremover.yaml
-python imageplotter.py -y imageplotter_after.yaml
+python imageplotter.py -y imageplotter.yaml --preserve_tmp --tmpdir $TMP
+python noiseremover.py -y noiseremover.yaml --preserve_tmp --tmpdir $TMP
+python imageplotter.py -y imageplotter_after.yaml --preserve_tmp --tmpdir $TMP
