88a7b8300895afb78cf15f6c589fc8827a855aec
Kenneth Dirker <kennethdirker@hotmail.nl>
2026-06-25 00:19:12 +0200

    use step context; WIP execution

--- transpile/Transpiler/mock_ups/process_images.py
+++ transpile/Transpiler/mock_ups/process_images.py
@@ -3,14 +3,21 @@ import dask, subprocess, sys, yaml
 from dask.distributed import Client
 
 # File dependent imports
-import glob
+from glob import glob
 from utils import FileObject, js_eval
 
-# # Typing imports
-# from typing import Any
 
 @dask.delayed
-def imageplotter(input_obj: dict, env: dict) -> dict:
+def imageplotter(input_obj: dict, context: dict) -> dict:
+    """
+    class: CommandLineTool
+    id: imageplotter
+    label: imageplotter
+    """
+    def outputs_output_glob():
+        context = {"inputs": inputs, **context}
+        return js_eval("inputs.output_image", context)
+
     cmd: list[str] = []
     outputs = {}
 
@@ -18,7 +25,7 @@ def imageplotter(input_obj: dict, env: dict) -> dict:
     inputs = {}
     inputs["input_fits"] = [FileObject(f) for f in input_obj["input_fits"]]
     inputs["output_image"] = str(input_obj["output_image"])
-
+    local_context = {"inputs": inputs, **context}
 
     # Build the command
     cmd = [
@@ -28,20 +35,21 @@ def imageplotter(input_obj: dict, env: dict) -> dict:
         inputs["output_image"]
     ]
 
-    print(" ".join(cmd))
-    
     subprocess.run(cmd)
-    outputs["output"] = FileObject(glob.glob(inputs["output_image"])[0])
+    outputs["output"] = FileObject(glob(outputs_output_glob(local_context))[0])
     return outputs
 
 
 @dask.delayed
-def noiseremover(input_obj: dict, env: dict) -> dict:
+def noiseremover(input_obj: dict, context: dict) -> dict:
     """
     class: CommandLineTool
     id: noiseremover
     label: noiseremover
     """
+    def outputs_output_glob(context: dict):
+        return js_eval("inputs.output_file_name", context)
+    
     cmd: list[str] = []
     outputs = {}
 
@@ -49,7 +57,7 @@ def noiseremover(input_obj: dict, env: dict) -> dict:
     inputs = {}
     inputs["input"] = FileObject(input_obj["input"])
     inputs["output_file_name"] = str(input_obj["output_file_name"])
-
+    local_context = {"inputs": inputs, **context}
 
     # Build the command
     cmd = [
@@ -60,41 +68,66 @@ def noiseremover(input_obj: dict, env: dict) -> dict:
     ]
     
     subprocess.run(cmd)
-    outputs["output"] = FileObject(glob.glob(inputs["output_file_name"])[0])
+    outputs["output"] = FileObject(glob(outputs_output_glob(local_context))[0])
     return outputs
 
+
 @dask.delayed
-def process_images(input_obj: dict, env: dict) -> dict:
+def process_images(input_obj: dict, context: dict) -> dict:
     """
     class: Workflow
     id: process_images
     label: process_images
     """
-    def noiseremover_input_valueFrom():
-        ...
-    def noiseremover_output_file_name_valueFrom():
-        ...
+    def noiseremover_input(context, self):
+        context["self"] = self
+        return js_eval("self[0]", context)
+    
+    def noiseremover_output_file_name(context, self):
+        context["self"] = self
+        return js_eval("'no_noise_' + inputs.input[0].basename", context)
+    
+    def after_plot_inspect(context, self):
+        context["self"] = self
+        return js_eval("[self]", context)
 
     outputs = {}
     inputs = {}
     inputs["fit_list"] = [FileObject(f) for f in input_obj["fit_list"]]
+    # local_context = {"inputs": inputs, **context} # Only needed when expressions are used outside of steps
+    step_context = context.copy()
 
+    # Step ID: imageplotter 
+    # Step label: imageplotter 
     imageplotter_inputs = {
         "input_fits": inputs["fit_list"],
         "output_image": "before_noise_remover.png"
     }
-    imageplotter_out = imageplotter(imageplotter_inputs, env)
+    imageplotter_out = imageplotter(imageplotter_inputs, context)
 
+    # Step ID: noiseremover
+    # Step label: noiseremover
     noiseremover_inputs = {
-        "input": noiseremover_input_valueFrom(),
-        
-        "output_file_name": noiseremover_output_file_name_valueFrom()
+        "input": inputs["fit_list"],
     }
-    noiseremover_out = noiseremover(noiseremover_inputs, env)
-    after_plot_inspect_out = imageplotter(after_plot_inspect_inputs, env)
+    step_context["inputs"] = {**inputs, **noiseremover_inputs}
+    noiseremover_inputs["input"] = noiseremover_input(step_context, inputs["fit_list"])
+    noiseremover_inputs["output_file_name"] = noiseremover_output_file_name(step_context, None)
+    noiseremover_out = noiseremover(noiseremover_inputs, context)
 
+    # Step ID: after_plot_inspect
+    # Step label: imageplotter
+    after_plot_inspect_inputs = {
+        "input_fits": noiseremover_out["output"],
+        "output_image": "after_noise_remover.png"
+    }
+    step_context["inputs"] = {**inputs, **after_plot_inspect_inputs}
+    after_plot_inspect_inputs["input_fits"] = after_plot_inspect(step_context, noiseremover_out["output"])
+    after_plot_inspect_out = imageplotter(after_plot_inspect_inputs, context)
 
-    outputs[""] = ...
+    # Export outputs
+    outputs["before_noise_remover"] = imageplotter_out["output"]
+    outputs["after_noise_remover_plot"] = after_plot_inspect_out["output"]
     return outputs
 
 
@@ -105,13 +138,11 @@ def main():
     with open(sys.argv[1], "r") as f:
         input_yaml = yaml.load(f, Loader=yaml.BaseLoader)
 
-    env = {}
+    context = {}
 
     # Submit to Dask
-    # future = client.submit(process_images, input_yaml, env)
-    # print(future.result())
-    t = process_images(input_yaml, env)
-    print(client.compute(t))
+    future = client.compute(process_images(input_yaml, context))
+    print(*[f'{k}: {v}' for k, v in future.result().items()])
 
 
 if __name__ == "__main__":
